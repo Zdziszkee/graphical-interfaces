2f537efcee69e5fa0afb04965d3649bd2a3bcdbd
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-12-08 20:39:32 +0100

    Change app theme

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,39 @@ import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const darkTheme = createTheme({
+const theme = createTheme({
   palette: {
-    mode: 'dark'
+    primary: {
+      main: '#cfb528', // Golden yellow
+      contrastText: '#FFFFFF', // White text for contrast
+    },
+    secondary: {
+      main: '#8B0000', // Dark red
+      contrastText: '#FFFFFF', // White text for contrast
+    },
+    text: {
+      primary: '#333333', // Dark gray for main text
+      secondary: '#555555', // Slightly lighter gray for secondary text
+    },
+    error: {
+      main: '#FF0000', // Red for error
+    },
+    success: {
+      main: '#008000', // Green for success
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif', // Default font
+    h1: { color: '#333333' }, // Ensure headers use the text color
+    h2: { color: '#333333' },
+    body1: { color: '#333333' },
+    body2: { color: '#555555' }, // For secondary text
   },
 });
 
-
 function App() {
   return (
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
             <ToastContainer />
           <BrowserRouter>
               <Routes>
