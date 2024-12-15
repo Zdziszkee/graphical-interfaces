538c004760596679ba6ea9e561c7a18e3bf74ca5
Kacper Kuchta <kacper.kuchta@here.com>
2024-12-15 22:39:43 +0100

    working photos for recipes

--- src/components/views/RecipesView.tsx
+++ src/components/views/RecipesView.tsx
@@ -1,29 +1,20 @@
-// import { List, ListItemButton, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
-// import ImageIcon from "@mui/icons-material/Image";
-//
-// const RecipesView = () => {
-//     const recipes = [
-//         {id: 1, name: "Medium rare steak", ingredients: [{name: "water", amount: "2 cups"}]}
-//     ]
-//
-//     return <List sx={{ width: '100%', maxWidth: 360}}>
-//         {["a", "b", "c"].map((elem, idx) => {
-//             return <ListItemButton key={idx}>
-//                 <ListItemAvatar>
-//                     <Avatar>
-//                         <ImageIcon />
-//                     </Avatar>
-//                 </ListItemAvatar>
-//                 <ListItemText primary={elem} secondary={elem} />
-//             </ListItemButton>})}
-//     </List>;
-// }
-//
-// export default RecipesView;
-
-// src/RecipeList.tsx
 import React, { useState } from 'react';
-import { List, ListItem, ListItemText, ListItemButton, Typography, Modal, Box, Button, IconButton, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import {
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+    Typography,
+    Modal,
+    Box,
+    Button,
+    IconButton,
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    Grid2, GridProps
+} from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { useAppContext } from '../../AppContext';
 
@@ -43,75 +34,96 @@ const RecipesView: React.FC = () => {
     };
 
     return (
-      <div>
-          <Typography variant="h4" gutterBottom>
-              Recipes
-          </Typography>
-          <List>
-              {recipes.map((recipe) => (
-                <ListItem key={recipe.id}>
-                    <ListItemButton onClick={() => handleOpen(recipe)}>
-                        <ListItemText primary={recipe.name} />
-                    </ListItemButton>
-                </ListItem>
-              ))}
-          </List>
+        <div>
+            <Typography variant="h4" gutterBottom>
+                Recipes
+            </Typography>
+            <Grid2 container spacing={2}>
+                {recipes.map((recipe) => (
+                    <Box
+                        key={recipe.id}
+                        sx={{
+                            gridColumn: { xs: 'span 12', sm: 'span 6', md: 'span 4', lg: 'span 3' },
+                        }}
+                    >
+                        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                            <CardActionArea onClick={() => handleOpen(recipe)} sx={{ flexGrow: 1 }}>
+                                <CardMedia
+                                    component="img"
+                                    sx={{
+                                        height: 140,
+                                        objectFit: 'cover',
+                                    }}
+                                    image={recipe.photo}
+                                    alt={recipe.name}
+                                />
+                                <CardContent>
+                                    <Typography variant="h6" component="div">
+                                        {recipe.name}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Box>
+                ))}
+            </Grid2>
 
-          <Modal open={open} onClose={handleClose}>
-              <Box
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 400,
-                    bgcolor: 'background.paper',
-                    p: 4,
-                    boxShadow: 24,
-                }}
-              >
-                  <IconButton onClick={handleClose} sx={{ position: 'absolute', top: 8, right: 8 }}>
-                      <CloseIcon />
-                  </IconButton>
-                  {selectedRecipe && (
-                    <>
-                        <Typography variant="h5">{selectedRecipe.name}</Typography>
-                        <Typography variant="h6" mt={2}>Ingredients:</Typography>
-                        <FormControl fullWidth>
-                            <InputLabel>Shopping List</InputLabel>
-                            <Select
-                              value={selectedShoppingList}
-                              onChange={(e) => selectShoppingList(e.target.value)}
-                              label="Shopping List"
-                            >
-                                {shoppingLists.map((list) => (
-                                  <MenuItem key={list.name} value={list.name}>
-                                      {list.name}
-                                  </MenuItem>
+
+            <Modal open={open} onClose={handleClose}>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        p: 4,
+                        boxShadow: 24,
+                    }}
+                >
+                    <IconButton onClick={handleClose} sx={{ position: 'absolute', top: 8, right: 8 }}>
+                        <CloseIcon />
+                    </IconButton>
+                    {selectedRecipe && (
+                        <>
+                            <Typography variant="h5">{selectedRecipe.name}</Typography>
+                            <Typography variant="h6" mt={2}>Ingredients:</Typography>
+                            <FormControl fullWidth>
+                                <InputLabel>Shopping List</InputLabel>
+                                <Select
+                                    value={selectedShoppingList}
+                                    onChange={(e) => selectShoppingList(e.target.value)}
+                                    label="Shopping List"
+                                >
+                                    {shoppingLists.map((list) => (
+                                        <MenuItem key={list.name} value={list.name}>
+                                            {list.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <ul>
+                                {selectedRecipe.ingredients.map((ingredient) => (
+                                    <li key={ingredient.name}>
+                                        {ingredient.name} - {ingredient.amount} {ingredient.unit}
+                                        <Button onClick={() => addToShoppingList(ingredient, selectedShoppingList)}>
+                                            Add to Shopping List
+                                        </Button>
+                                    </li>
                                 ))}
-                            </Select>
-                        </FormControl>
-                        <ul>
-                            {selectedRecipe.ingredients.map((ingredient) => (
-                              <li key={ingredient.name}>
-                                  {ingredient.name} - {ingredient.amount} {ingredient.unit}
-                                  <Button onClick={() => addToShoppingList(ingredient, selectedShoppingList)}>
-                                      Add to Shopping List
-                                  </Button>
-                              </li>
-                            ))}
-                        </ul>
-                        <Typography variant="h6" mt={2}>Steps:</Typography>
-                        <ol>
-                            {selectedRecipe.steps.map((step, index) => (
-                              <li key={index}>{step}</li>
-                            ))}
-                        </ol>
-                    </>
-                  )}
-              </Box>
-          </Modal>
-      </div>
+                            </ul>
+                            <Typography variant="h6" mt={2}>Steps:</Typography>
+                            <ol>
+                                {selectedRecipe.steps.map((step, index) => (
+                                    <li key={index}>{step}</li>
+                                ))}
+                            </ol>
+                        </>
+                    )}
+                </Box>
+            </Modal>
+        </div>
     );
 };
 
